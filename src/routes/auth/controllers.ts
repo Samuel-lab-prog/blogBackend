6564bce27a6d62ba09c2ebf349838ad3f11c495b
Samuel <sg1976387@gmail.com>
2025-12-27 09:25:27 -0300

    fix bug

--- src/routes/auth/controllers.ts
+++ src/routes/auth/controllers.ts
@@ -4,65 +4,67 @@ import { authPlugin, SetupPlugin } from '@utils';
 import { loginUser } from './services';
 
 export const authRouter = new Elysia().group('/auth', (app) =>
-	app
-		.use(SetupPlugin)
-		.post(
-			'/login',
-			async ({ body, cookie, set, store }) => {
-				const authInitiated = performance.now();
+  app
+    .use(SetupPlugin)
+    .post(
+      '/login',
+      async ({ body, cookie, set, store }) => {
+        const authInitiated = performance.now();
 
-				const result = await loginUser(body.email, body.password);
+        const result = await loginUser(body.email, body.password);
 
-				cookie.token!.value = result.token;
-				cookie.token!.httpOnly = true;
-				cookie.token!.path = '/';
-				cookie.token!.maxAge = 60 * 60 * 24 * 7; // 7 days
+        cookie.token!.value = result.token;
+        cookie.token!.httpOnly = true;
+        cookie.token!.path = '/';
+        cookie.token!.maxAge = 60 * 60 * 24 * 7; // 7 days
+        cookie.token!.secure = process.env.NODE_ENV === 'production';
+        cookie.token!.sameSite = 'none';
 
-				log.debug('Cookie successfully set for login route');
-				set.status = 204;
+        log.debug('Cookie successfully set for login route');
+        set.status = 204;
 
-				store.userId = result.data.id;
-				store.role = 'admin';
-				store.authTiming = Math.round(performance.now() - authInitiated);
+        store.userId = result.data.id;
+        store.role = 'admin';
+        store.authTiming = Math.round(performance.now() - authInitiated);
 
-				return;
-			},
-			{
-				body: loginSchema,
-				response: {
-					204: t.Void(),
-					400: appErrorSchema,
-					401: appErrorSchema,
-					422: appErrorSchema,
-					500: appErrorSchema,
-				},
-				detail: {
-					summary: 'Login',
-					description:
-						'Authenticates a user and returns a JWT token in an HTTP-only cookie.',
-					tags: ['Auth'],
-				},
-			},
-		)
-		.use(authPlugin)
-		.post(
-			'',
-			({ set }) => {
-				// Dummy route to apply authPlugin
-				set.status = 204;
-				return;
-			},
-			{
-				response: {
-					204: t.Void(),
-					401: appErrorSchema,
-					500: appErrorSchema,
-				},
-				detail: {
-					summary: 'Authenticated Route',
-					description: 'A dummy protected route to verify authentication.',
-					tags: ['Auth'],
-				},
-			},
-		),
+        return;
+      },
+      {
+        body: loginSchema,
+        response: {
+          204: t.Void(),
+          400: appErrorSchema,
+          401: appErrorSchema,
+          422: appErrorSchema,
+          500: appErrorSchema,
+        },
+        detail: {
+          summary: 'Login',
+          description:
+            'Authenticates a user and returns a JWT token in an HTTP-only cookie.',
+          tags: ['Auth'],
+        },
+      },
+    )
+    .use(authPlugin)
+    .post(
+      '',
+      ({ set }) => {
+        // Dummy route to apply authPlugin
+        set.status = 204;
+        return;
+      },
+      {
+        response: {
+          204: t.Void(),
+          401: appErrorSchema,
+          500: appErrorSchema,
+        },
+        detail: {
+          summary: 'Authenticated Route',
+          description: 'A dummy protected route to verify authentication.',
+          tags: ['Auth'],
+        },
+      },
+    ),
 );
